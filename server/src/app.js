acafddd2d50e362930bc2e5d216e9fa9a78f4172
Jalal Khan <jmankhan1@gmail.com>
2022-07-01 19:02:05 -0400

    remove boilerplate

--- server/src/app.js
+++ server/src/app.js
@@ -39,7 +39,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', process.env.WHITELISTED_DOMAINS || 'localhost:3000');
   res.setHeader('Access-Control-Allow-Credentials', true);
